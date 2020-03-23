21ff229410c22d4d65aa088c6dfb197c25c35122
minwoossong1 <smw_1000@naver.com>
2020-03-23 18:30:28 +0900

    수정

---
+++ dev_book/src/hotfix/HotFixTestName4.java
@@ -0,0 +1,11 @@
+package hotfix;
+
+public class HotFixTestName4 {
+
+	public static void main(String[] args) {
+	//hotfix 브랜치에서 4번쨰로 추가한 소스임.
+		System.out.println("hotfix 브랜치에서 4번쨰로 추가한 소스임.");
+
+	}
+
+}
