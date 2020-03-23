c41f1e5005913e3d8f562e43b6bd89b3442d9749
minwoossong1 <smw_1000@naver.com>
2020-03-23 15:43:44 +0900

    hotfix 브랜치로 파일 추가함. 03:43

---
+++ dev_book/src/hitfix/HotFixTestName.java
@@ -0,0 +1,11 @@
+package hitfix;
+
+public class HotFixTestName {
+
+	public static void main(String[] args) {
+	//프로젝트명->오른쪽->remote->switch->new branch->hotfix
+		System.out.println("hotfix 브랜치에서 추가한 소스임.");
+
+	}
+
+}
