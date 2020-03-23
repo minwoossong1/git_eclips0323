ddf1d6cd4b7e44d9ce67d724b8ca7865d8595286
minwoossong1 <smw_1000@naver.com>
2020-03-23 16:27:20 +0900

    1번째 파일 다시 올리기

--- dev_book/src/hitfix/HotFixTestName.java
+++ dev_book/src/hitfix/HotFixTestName.java
@@ -3,7 +3,7 @@ package hitfix;
 public class HotFixTestName {
 
 	public static void main(String[] args) {
-	//프로젝트명->오른쪽->remote->switch->new branch->hotfix
+		//프로젝트명->오른쪽->remote->switch->new branch->hotfix
 		System.out.println("hotfix 브랜치에서 추가한 소스임.");
 
 	}
