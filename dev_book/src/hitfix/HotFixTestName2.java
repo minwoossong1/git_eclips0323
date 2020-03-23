026cf1663e7824e827657d477f828ff73a278903
minwoossong1 <smw_1000@naver.com>
2020-03-23 15:56:52 +0900

    hotfix 브랜치에서 2번쨰로 추가한 소스임.

---
+++ dev_book/src/hitfix/HotFixTestName2.java
@@ -0,0 +1,11 @@
+package hitfix;
+
+public class HotFixTestName2 {
+
+	public static void main(String[] args) {
+	//hotfix 브랜치에서 2번쨰로 추가한 소스임.
+		System.out.println("hotfix 브랜치에서 2번쨰로 추가한 소스임.");
+
+	}
+
+}
