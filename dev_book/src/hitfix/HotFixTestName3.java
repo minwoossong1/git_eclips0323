5599cc18f714d146e5753a62aca22ab780f97fb6
minwoossong1 <smw_1000@naver.com>
2020-03-23 16:42:24 +0900

    3번째 추가

---
+++ dev_book/src/hitfix/HotFixTestName3.java
@@ -0,0 +1,11 @@
+package hitfix;
+
+public class HotFixTestName3 {
+
+	public static void main(String[] args) {
+	//hotfix 브랜치에서 3번쨰로 추가한 소스임.
+		System.out.println("hotfix 브랜치에서 3번쨰로 추가한 소스임.");
+
+	}
+
+}
